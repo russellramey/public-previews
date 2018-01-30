cfda0015b6f22a8fd26289aa698b591632c56c2f
Russell Ramey <doublercreative@gmail.com>
2018-01-30 09:14:06 -0600

    initial commit

---
+++ public-preview.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Plugin Name: WP Public Preview
+* Plugin URI: http://russellramey.me/wordpress/wp-no-comments
+* Description: Enables non logged in users to preview posts, pages, or other content types in draft status.
+* Version: 1.0
+* Author: Russell Ramey
+* Author URI: http://russellramey.me/
+*/
+
+
+?>
