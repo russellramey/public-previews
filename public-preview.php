90f32c2ae5ece8e2a5e8c27dfebbad6e6aeea4d2
Russell Ramey <doublercreative@gmail.com>
2018-01-31 14:54:35 -0600

    cleaned up functions. added support for specific post types.

--- public-preview.php
+++ public-preview.php
@@ -8,55 +8,77 @@
 * Author URI: http://russellramey.me/
 */
 
-// Add wp_public_preview function to wp post_results
+// Add preview action to sleceted post types
 add_filter( 'posts_results', 'wp_public_previews', null, 2 );
 function wp_public_previews( $posts, &$query ) {
 
-	// If more than 1 post is returned
-    if ( sizeof( $posts ) != 1 ) return $posts; /* not interested */
 
-	// Get post status
+	// Get post type 
+	$type_options = array("post", "page");
+	$type = get_post_type( $posts[0] );
+
+	// Get post status & set status object
     $status = get_post_status( $posts[0] );
     $post_status_obj = get_post_status_object( $status );
 
-	// If post is public, return as normal
-    if ( $post_status_obj->public ) return $posts; /* it's public */
 
-	// If post is draft, and ?_publicpreview=true
-    if ( !isset( $_GET['_publicpreview'] ) || $_GET['_publicpreview'] != 'true' )
-        return $posts; /* not for your eyes */
+	// If more than 1 post is returned, return normally
+    if ( sizeof($posts) != 1 ) {
+    	return $posts;
+    } 
 
-	// Store post query
-    $query->_public_preview_stash = $posts; /* stash away */
+	// If post status is public, return as normal as it is public
+    if ( $post_status_obj->public ) {
+    	return $posts;
+    }
 
-	// Remove filter
-    add_filter( 'the_posts', 'wp_public_previews_inject_private', null, 2 );
-	function wp_public_previews_inject_private( $posts, &$query ) {
-	    /* do only once */
-	    remove_filter( 'the_posts', 'wp_public_previews_inject_private', null, 2 );
-	    return $query->_public_preview_stash;
+    // If post is draft, and ?_publicpreview=true
+    if (!isset( $_GET['_publicpreview'] ) || $_GET['_publicpreview'] != 'true' ) {
+		 return $posts;
+    }
+
+	// Store main post query
+    $query->_public_preview_cache = $posts; /* stash away */
+
+    // If correct post type
+    if(in_array($type, $type_options)){
+
+		// Remove filter
+	    add_filter( 'the_posts', 'wp_public_previews_active', null, 2 );
+		function wp_public_previews_active( $posts, &$query ) {
+		    /* do only once */
+		    remove_filter( 'the_posts', 'wp_public_previews_active', null, 2 );
+		    return $query->_public_preview_cache;
+		}
+
 	}
+	
 }
 
 // Add metabox to display valid preview link
-add_action( 'add_meta_boxes', 'wp_public_preview_output' );
-function wp_public_preview_output() {
+add_action( 'add_meta_boxes', 'wp_public_preview_metabox' );
+function wp_public_preview_metabox($post) {
+
+	// Global post var
 	global $post;
+	
 	// Get post status
     $status = get_post_status($post->ID);
     $post_status_obj = get_post_status_object( $status );
+
 
 	// If post is not published show metabox
     if ( $status != 'publish' ) {
 		// Add metabox
 		add_meta_box('preview_output_code', 'Public Preview', 'preview_output_code', 'post', 'side', 'default');
+		
 	}
 
-	// Display public preview url in metabox
-	function preview_output_code() {
-		global $post;
+	// Create content and output for metabox
+	function preview_output_code($post) {
+
 		echo '<input style="width:100%" type="text" value="' . get_bloginfo('url') . '/?p=' . $post->ID  . '&_publicpreview=true" />';
-		echo '<p><i>Use this link to provide a public preview link. This will allow someone to view "Draft" status posts without loggin in.</i></p>';
+		echo '<p><i>Use this link to provide a public preview link. This will allow someone to view "Draft" status posts without a login.</i></p>';
 	}
 }
 ?>
